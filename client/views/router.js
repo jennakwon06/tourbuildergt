1f3ac9d032c70b1f2017b79170a247eeab5148d6
Jenna Kwon <jinkwon06@gmail.com>
2016-04-16 12:33:21 -0400

    Basic barebone

---
+++ client/views/router.js
@@ -0,0 +1,29 @@
+Router.configure({
+	templateNameConverter: "upperCamelCase",
+	routeControllerNameConverter: "upperCamelCase",
+	layoutTemplate: "layout",
+	notFoundTemplate: "notFound",
+	loadingTemplate: "loading"
+});
+
+var freeRoutes = [
+	"home",
+	"about"
+];
+
+Router.onBeforeAction(function() {
+	// loading indicator here
+	if(!this.ready()) {
+		$("body").addClass("wait");
+		this.render("loading");
+	} else {
+		$("body").removeClass("wait");
+		this.next();
+	}
+});
+
+Router.map(function () {
+
+	this.route("home", {path: "/", controller: "HomeController"});
+	this.route("about", {path: "/about", controller: "AboutController"});
+});
